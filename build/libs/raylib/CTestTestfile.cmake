22739492c5b2ab84766673ab724f225dd0fd34aa
Avginike <janjuriewicz@gmail.com>
2023-12-29 19:22:38 +0100

    removed build folder

--- build/libs/raylib/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/janek/Projects/BlatFormer/libs/raylib
-# Build directory: C:/Users/janek/Projects/BlatFormer/build/libs/raylib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("raylib")
